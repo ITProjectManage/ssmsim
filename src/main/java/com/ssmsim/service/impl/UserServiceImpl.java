ac8289638180db9e701db70e4dca9f34165f5ccc
chungForChen <303956722@qq.com>
2017-12-24 12:49:58 +0800

    任课老师
    任课老师出版

--- src/main/java/com/ssmsim/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssmsim/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssmsim.service.impl;
 import com.ssmsim.dao.IUserDao;
 import com.ssmsim.model.User;
 import com.ssmsim.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
